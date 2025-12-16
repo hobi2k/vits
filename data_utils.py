f7e66c39ff0e0dcd3107bc41d95e1c1d248bb2ae
Hosung <ahnhs2k@gmail.com>
2025-12-17 08:40:58 +0900

    fix: PyTorch cu128 compatibility (stft, dataloader, plotting)

--- data_utils.py
+++ data_utils.py
@@ -4,6 +4,7 @@ import random
 import numpy as np
 import torch
 import torch.utils.data
+import torchaudio
 
 import commons 
 from mel_processing import spectrogram_torch
@@ -200,22 +201,43 @@ class TextAudioSpeakerLoader(torch.utils.data.Dataset):
         sid = self.get_sid(sid)
         return (text, spec, wav, sid)
 
+
     def get_audio(self, filename):
         audio, sampling_rate = load_wav_to_torch(filename)
+
+        # 리샘플링 (필수)
         if sampling_rate != self.sampling_rate:
-            raise ValueError("{} {} SR doesn't match target {} SR".format(
-                sampling_rate, self.sampling_rate))
+            audio = torchaudio.functional.resample(
+                audio,
+                orig_freq=sampling_rate,
+                new_freq=self.sampling_rate
+            )
+            sampling_rate = self.sampling_rate
+
         audio_norm = audio / self.max_wav_value
         audio_norm = audio_norm.unsqueeze(0)
+
         spec_filename = filename.replace(".wav", ".spec.pt")
         if os.path.exists(spec_filename):
             spec = torch.load(spec_filename)
         else:
-            spec = spectrogram_torch(audio_norm, self.filter_length,
-                self.sampling_rate, self.hop_length, self.win_length,
-                center=False)
-            spec = torch.squeeze(spec, 0)
+            spec = spectrogram_torch(
+                audio_norm,
+                self.filter_length,
+                self.sampling_rate,
+                self.hop_length,
+                self.win_length,
+                center=False
+            )
+
+            # 안전한 차원 보정
+            if spec.dim() == 3:
+                spec = spec[0]
+            elif spec.dim() != 2:
+                raise RuntimeError(f"Invalid spec shape: {spec.shape}")
+
             torch.save(spec, spec_filename)
+
         return spec, audio_norm
 
     def get_text(self, text):
@@ -246,52 +268,56 @@ class TextAudioSpeakerCollate():
         self.return_ids = return_ids
 
     def __call__(self, batch):
-        """Collate's training batch from normalized text, audio and speaker identities
-        PARAMS
-        ------
-        batch: [text_normalized, spec_normalized, wav_normalized, sid]
-        """
-        # Right zero-pad all one-hot text sequences to max input length
-        _, ids_sorted_decreasing = torch.sort(
-            torch.LongTensor([x[1].size(1) for x in batch]),
-            dim=0, descending=True)
 
-        max_text_len = max([len(x[0]) for x in batch])
+        # spec shape 방어
+        for i, x in enumerate(batch):
+            spec = x[1]
+            if spec.dim() != 2:
+                raise RuntimeError(
+                    f"Invalid spec shape at batch index {i}: {spec.shape}"
+                )
+
+        # text 길이 기준 정렬 (안전)
+        _, ids_sorted_decreasing = torch.sort(
+            torch.LongTensor([x[0].size(0) for x in batch]),
+            dim=0, descending=True
+        )
+
+        max_text_len = max([x[0].size(0) for x in batch])
         max_spec_len = max([x[1].size(1) for x in batch])
-        max_wav_len = max([x[2].size(1) for x in batch])
+        max_wav_len  = max([x[2].size(1) for x in batch])
 
         text_lengths = torch.LongTensor(len(batch))
         spec_lengths = torch.LongTensor(len(batch))
-        wav_lengths = torch.LongTensor(len(batch))
+        wav_lengths  = torch.LongTensor(len(batch))
         sid = torch.LongTensor(len(batch))
 
-        text_padded = torch.LongTensor(len(batch), max_text_len)
-        spec_padded = torch.FloatTensor(len(batch), batch[0][1].size(0), max_spec_len)
-        wav_padded = torch.FloatTensor(len(batch), 1, max_wav_len)
-        text_padded.zero_()
-        spec_padded.zero_()
-        wav_padded.zero_()
+        n_mel = batch[0][1].size(0)
+
+        text_padded = torch.zeros(len(batch), max_text_len, dtype=torch.long)
+        spec_padded = torch.zeros(len(batch), n_mel, max_spec_len)
+        wav_padded  = torch.zeros(len(batch), 1, max_wav_len)
+
         for i in range(len(ids_sorted_decreasing)):
             row = batch[ids_sorted_decreasing[i]]
 
-            text = row[0]
+            text, spec, wav, spk = row
+
             text_padded[i, :text.size(0)] = text
             text_lengths[i] = text.size(0)
 
-            spec = row[1]
             spec_padded[i, :, :spec.size(1)] = spec
             spec_lengths[i] = spec.size(1)
 
-            wav = row[2]
             wav_padded[i, :, :wav.size(1)] = wav
             wav_lengths[i] = wav.size(1)
 
-            sid[i] = row[3]
+            sid[i] = spk
 
         if self.return_ids:
             return text_padded, text_lengths, spec_padded, spec_lengths, wav_padded, wav_lengths, sid, ids_sorted_decreasing
-        return text_padded, text_lengths, spec_padded, spec_lengths, wav_padded, wav_lengths, sid
 
+        return text_padded, text_lengths, spec_padded, spec_lengths, wav_padded, wav_lengths, sid
 
 class DistributedBucketSampler(torch.utils.data.distributed.DistributedSampler):
     """
--- utils.py
+++ utils.py
@@ -99,8 +99,9 @@ def plot_spectrogram_to_numpy(spectrogram):
   plt.tight_layout()
 
   fig.canvas.draw()
-  data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
-  data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+
+  buf = np.asarray(fig.canvas.buffer_rgba())
+  data = buf[:, :, :3]  # RGBA -> RGB
   plt.close()
   return data
 
@@ -128,8 +129,9 @@ def plot_alignment_to_numpy(alignment, info=None):
   plt.tight_layout()
 
   fig.canvas.draw()
-  data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
-  data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+
+  buf = np.asarray(fig.canvas.buffer_rgba())
+  data = buf[:, :, :3]  # RGBA -> RGB
   plt.close()
   return data
 
